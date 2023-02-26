6fd672d6730863b6182d8b75137dc291a093a2d3
beratdogan010 <58510412+beratdogan010@users.noreply.github.com>
2023-02-26 20:33:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -95,7 +95,7 @@ btnHold.addEventListener('click',function(){
 
 
     //2.Check if player's score is>=100
-    if(scores[activePlayer] >=20){
+    if(scores[activePlayer] >=100){
         playing = false;
         diceElm.classList.add('hidden');
 
